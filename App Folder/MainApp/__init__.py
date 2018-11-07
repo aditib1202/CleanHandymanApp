1d020cd32eb4392c046aa1f527628bb43dc02a66
CimaThaqi <13sann@protonmail.com>
2018-11-07 18:50:57 +0530

    admin-candidate commit

--- App Folder/MainApp/__init__.py
+++ App Folder/MainApp/__init__.py
@@ -32,4 +32,10 @@ def create_app(test_config=None):
 	from . import auth
 	app.register_blueprint(auth.bp)
 
+	from . import admin
+	app.register_blueprint(admin.bp)
+
+	from . import candidate
+	app.register_blueprint(candidate.bp)
+
 	return app
